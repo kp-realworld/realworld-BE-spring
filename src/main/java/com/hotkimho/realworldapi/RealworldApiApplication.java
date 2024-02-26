830e23fed4877efb7f0332bf58eb88375c31cb78
kimho <rlaghekd777@naver.com>
2024-02-27 00:23:39 +0900

    # ADD - init spring boot project

---
+++ src/main/java/com/hotkimho/realworldapi/RealworldApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotkimho.realworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldApiApplication.class, args);
+	}
+
+}
